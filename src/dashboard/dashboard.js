e63497d1b09d5561e369052eb838a304a01d9bea
Pierce Hicks <phicks21@gmail.com>
2019-11-11 20:11:59 -0500

    chat display is now setup

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,11 +1,78 @@
 import React from 'react';
+import ChatListComponent from '../chatlist/chatList';
+import { Button, withStyles } from '@material-ui/core';
+import styles from './styles';
+import ChatViewComponent from '../chatview/chatView';
+
+const firebase = require('firebase');
 
 class DashboardComponent extends React.Component {
 
+    constructor(){
+      super();
+        this.state = {
+          selectedChat: null,
+          newChatFormVisible: false,
+          email: null,
+          chats: []
+        };
+    }
+
   render() {
-    return(<div>Hello World From Dashboard Component</div>);
+
+    const { classes } = this.props;
+
+    return(
+      <div>
+      <ChatListComponent
+        history={this.props.history}
+        newChatBtn={this.newChatBtnClicked}
+        selectChatFn={this.selectChat}
+        chats={this.state.chats}
+        userEmail={this.state.email}
+        selectedChatIndex={this.state.selectedChat}>
+        </ChatListComponent>
+        {
+          this.state.newChatFormVisible ?
+          null :
+          <ChatViewComponent
+            user={this.state.email}
+            chat={this.state.chats[this.state.selectedChat]}>
+          </ChatViewComponent>
+        }
+        <Button className={classes.signOutBtn} onClick={this.signOut}>Sign Out</Button>
+      </div>
+    );
   }
+
+  signOut = () => firebase.auth().signOut();
+
+  selectChat = (chatIndex) => {
+    this.setState({ selectedChat: chatIndex });
+  }
+
+  newChatBtnClicked = () => this.setState({ newChatFormVisible: true, selectedChat: null });
+
+componentDidMount = () => {
+  firebase.auth().onAuthStateChanged(async _usr => {
+    if(!_usr)
+      this.props.history.push('/login');
+    else {
+      await firebase
+        .firestore()
+        .collection('chats')
+        .where('users', 'array-contains', _usr.email)
+        .onSnapshot(async res => {
+          const chats = res.docs.map(_doc => _doc.data())
+          await this.setState({
+            email: _usr.email,
+            chats: chats
+          })
+        })
+    }
+  })
+}
 
 }
 
-export default DashboardComponent;
+export default withStyles(styles) (DashboardComponent);
